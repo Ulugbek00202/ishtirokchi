845e8c1f61bcb667c3ae71933ff076d30d5dbf8c
unknown <kreede929@gmail.com>
2024-09-18 12:20:57 +0500

    new page

---
+++ script.js
@@ -0,0 +1,141 @@
+const birinchi_name = prompt('Ism kiriting')
+const birinchi_age = +prompt('Yoshingizni kiriting')
+
+const ikkinchi_name = prompt('Ism kiriting')
+const ikkinchi_age = +prompt('Yoshingizni kiriting')
+
+const uchinchi_name = prompt('Ism kiriting')
+const uchinchi_age = +prompt('Yoshingizni kiriting')
+
+const tortinchi_name = prompt('Ism kiriting')
+const tortinchi_age = +prompt('Yoshingizni kiriting')
+
+const beshinchi_name = prompt('Ism kiriting')
+const beshinchi_age = +prompt('Yoshingizni kiriting')
+
+const oltinchi_name = prompt('Ism kiriting')
+const oltinchi_age = +prompt('Yoshingizni kiriting')
+
+const yettinchi_name = prompt('Ism kiriting')
+const yettinchi_age = +prompt('Yoshingizni kiriting')
+
+const sakkizinchi_name = prompt('Ism kiriting')
+const sakkizinchi_age = +prompt('Yoshingizni kiriting')
+
+const toqqizinchi_name = prompt('Ism kiriting')
+const toqqizinchi_age = +prompt('Yoshingizni kiriting')
+
+const oninchi_name = prompt('Ism kiriting')
+const oninchi_age = +prompt('Yoshingizni kiriting')
+
+const ishtirokchi1 = {
+	ishtirokchi: '1 - Ishtirokchi',
+	name: birinchi_name,
+	age: birinchi_age,
+}
+console.log(ishtirokchi1.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi1.name}`)
+console.log(`Sizning Yoshingiz ${ishtirokchi1.age}`)
+
+const ishtirokchi2 = {
+	ishtirokchi: '2 - Ishtirokchi',
+	name: ikkinchi_name,
+	age: ikkinchi_age,
+}
+
+console.log(ishtirokchi2.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi2.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi2.age}`)
+
+const ishtirokchi3 = {
+	ishtirokchi: '3 - Ishtirokchi',
+	name: uchinchi_name,
+	age: uchinchi_age,
+}
+
+console.log(ishtirokchi3.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi3.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi3.age}`)
+
+const ishtirokchi4 = {
+	ishtirokchi: '4 - Ishtirokchi',
+	name: tortinchi_name,
+	age: tortinchi_age,
+}
+
+console.log(ishtirokchi4.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi4.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi4.age}`)
+
+const ishtirokchi5 = {
+	ishtirokchi: '5 - Ishtirokchi',
+	name: beshinchi_name,
+	age: beshinchi_age,
+}
+
+console.log(ishtirokchi5.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi5.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi5.age}`)
+
+const ishtirokchi6 = {
+	ishtirokchi: '6 - Ishtirokchi',
+	name: oltinchi_name,
+	age: oltinchi_age,
+}
+
+console.log(ishtirokchi6.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi6.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi6.age}`)
+
+const ishtirokchi7 = {
+	ishtirokchi: '7 - Ishtirokchi',
+	name: yettinchi_name,
+	age: yettinchi_age,
+}
+
+console.log(ishtirokchi7.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi7.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi7.age}`)
+
+const ishtirokchi8 = {
+	ishtirokchi: '8 - Ishtirokchi',
+	name: sakkizinchi_name,
+	age: sakkizinchi_age,
+}
+
+console.log(ishtirokchi8.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi8.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi8.age}`)
+
+const ishtirokchi9 = {
+	ishtirokchi: '9 - Ishtirokchi',
+	name: toqqizinchi_name,
+	age: toqqizinchi_age,
+}
+
+console.log(ishtirokchi9.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi9.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi9.age}`)
+
+const ishtirokchi10 = {
+	ishtirokchi: '10 - Ishtirokchi',
+	name: oninchi_name,
+	age: oninchi_age,
+}
+
+console.log(ishtirokchi10.ishtirokchi)
+console.log(`Sizning ismingiz: ${ishtirokchi10.name}`)
+console.log(`Sizning ismingiz: ${ishtirokchi10.age}`)
+
+// console.log(
+// 	ishtirokchi1,
+// 	ishtirokchi2,
+// 	ishtirokchi3,
+// 	ishtirokchi4,
+// 	ishtirokchi5,
+// 	ishtirokchi6,
+// 	ishtirokchi7,
+// 	ishtirokchi8,
+// 	ishtirokchi9,
+// 	ishtirokchi10
+// )
